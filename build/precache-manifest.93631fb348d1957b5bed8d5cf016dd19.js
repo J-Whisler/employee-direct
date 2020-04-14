c8a29314e5a6d54b9c5d1e2c6ffd004d887cc711
Jacob Whisler <jwhisler521@gmail.com>
2020-04-13 20:56:46 -0400

    bug fix

--- build/precache-manifest.93631fb348d1957b5bed8d5cf016dd19.js
+++ build/precache-manifest.93631fb348d1957b5bed8d5cf016dd19.js
@@ -1,22 +1,22 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "b8f6c7da11de815a85477203f627c695",
-    "url": "/index.html"
+    "url": "/employee-direct/index.html"
   },
   {
     "revision": "6dc08f3db9b76f49135e",
-    "url": "/static/css/main.1a45bae8.chunk.css"
+    "url": "/employee-direct/static/css/main.1a45bae8.chunk.css"
   },
   {
     "revision": "98d381b8539990c9da13",
-    "url": "/static/js/2.88f9d077.chunk.js"
+    "url": "/employee-direct/static/js/2.88f9d077.chunk.js"
   },
   {
     "revision": "6dc08f3db9b76f49135e",
-    "url": "/static/js/main.18b8a935.chunk.js"
+    "url": "/employee-direct/static/js/main.18b8a935.chunk.js"
   },
   {
     "revision": "42ac5946195a7306e2a5",
-    "url": "/static/js/runtime~main.a8a9905a.js"
+    "url": "/employee-direct/static/js/runtime~main.a8a9905a.js"
   }
 ]);
