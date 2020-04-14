a003aa2286d96638fdcf99720f531066ecc04699
Jacob Whisler <jwhisler521@gmail.com>
2020-04-13 21:31:48 -0400

    fixes

--- build/precache-manifest.a997bce04eeaa208469061fa3a7f7e39.js
+++ build/precache-manifest.a997bce04eeaa208469061fa3a7f7e39.js
@@ -1,26 +1,26 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "f30df0a753f92614fa2bec2c9fd4a672",
-    "url": "index.html"
+    "url": "/employee-direct/index.html"
   },
   {
     "revision": "0b5e0070ead7e6c8db25",
-    "url": "/static/css/main.1a45bae8.chunk.css"
+    "url": "/employee-direct/static/css/main.1a45bae8.chunk.css"
   },
   {
     "revision": "06611d205afe6343c854",
-    "url": "/static/js/2.717472a9.chunk.js"
+    "url": "/employee-direct/static/js/2.717472a9.chunk.js"
   },
   {
     "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
-    "url": "/static/js/2.717472a9.chunk.js.LICENSE.txt"
+    "url": "/employee-direct/static/js/2.717472a9.chunk.js.LICENSE.txt"
   },
   {
     "revision": "0b5e0070ead7e6c8db25",
-    "url": "/static/js/main.236020a8.chunk.js"
+    "url": "/employee-direct/static/js/main.236020a8.chunk.js"
   },
   {
     "revision": "26d3d91965ef81de38e6",
-    "url": "/static/js/runtime-main.44ca452b.js"
+    "url": "/employee-direct/static/js/runtime-main.44ca452b.js"
   }
 ]);
